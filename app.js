cc2ed7b0a87e7938fcb295bfd18a79f6619d0b36
zeteo_xulon <n.dreux@live.fr>
2021-10-27 19:24:22 +0200

    last commit

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ app.delete("/api/products/:id", (req, res, next) => {
 
 app.get("/api/products/:id", (req, res, next) => {
 	Product.findOne({ _id: req.params.id })
-		.then((product) => res.status(200).json({product: product}))
+		.then((Product) => res.status(200).json({product: Product}))
 		.catch((error) => res.status(404).json({ error }));
 });
 
 app.get("/api/products", (req, res, next) => {
 	Product.find()
-		.then((product) => res.status(200).json({products: product}))
+		.then((Product) => res.status(200).json({products: Product}))
 		.catch((error) => res.status(400).json({ error }));
 });
 
