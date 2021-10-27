4f14ee2466cb2c42d3c7dd66fa4b324cccb8dc8e
zeteo_xulon <n.dreux@live.fr>
2021-10-27 17:42:52 +0200

    Problème sur product : product réglé. Le P6 il faut obéir pratiquement à la lettre

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Product = require("./models/Product");
 
+
+mongoose
+	.connect(
+		"mongodb+srv://pomme:apple1234@cluster0.pk40z.mongodb.net/fullstack_activity?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log("Connexion à MongoDB Atlas réussie !"))
+	.catch(() => console.log("Connexion à MongoDB Atlas échouée !"));
+
+// toujours après mongoose.connect
 const app = express();
 
-mongoose.connect('mongodb+srv://fullstack:fullLearning@cluster0.pk40z.mongodb.net/gofullstack?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+	next();
 });
 
-app.use((req, res, next) => {
-	res.json({ message: "La requête à fonctionné." });
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/api/products', (req, res, next) => {
+  // delete req.body._id;
+  const product = new Product({
+    ...req.body
+  });
+  product.save()
+    .then((product) => res.status(201).json({ product }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put("/api/products/:id", (req, res, next) => {
+	Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Modified!" }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/products/:id", (req, res, next) => {
+	Product.deleteOne({ _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Deleted !" }))
+		.catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/products/:id", (req, res, next) => {
+	Product.findOne({ _id: req.params.id })
+		.then((product) => res.status(200).json({product: product}))
+		.catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/products", (req, res, next) => {
+	Product.find()
+		.then((product) => res.status(200).json({products: product}))
+		.catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;
+
+// Zeteo_Xulon
